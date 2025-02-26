02037c0095b65ebfd3823b449590b7ba822e5f8d
alco <you@example.com>
2025-02-26 13:45:04 +0400

    Init

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,15 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+# 
+output "subnet_ids" {
+  description = "List of Subnet ID's"
+  value = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
+}
+
 # This is output structure is a map with list values.
 output "public_subnet" {
-  description = "VPC Public Subnets"
+  description = "Map of Public Subnet IDs list and Public Subnet AZs list"
   value = {
      ID = aws_subnet.public[*].id
      AZName = aws_subnet.public[*].availability_zone
